ae1ec3a0c20bc9ee8a2a15363014266ef082732c
Kevin N <nkvn98@gmail.com>
2019-11-03 08:14:55 -0500

    Updates

---
+++ precache-manifest.a9236d37c1a8886a5d3b001c1f2a5af8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ec056938c42ae0136a6ae8027be5283",
+    "url": "/foodiesah/index.html"
+  },
+  {
+    "revision": "0d763c17c8b51efb8199",
+    "url": "/foodiesah/static/css/main.61f5677f.chunk.css"
+  },
+  {
+    "revision": "d97dff6facc50bf192b0",
+    "url": "/foodiesah/static/js/2.09e08d19.chunk.js"
+  },
+  {
+    "revision": "0d763c17c8b51efb8199",
+    "url": "/foodiesah/static/js/main.0f03f71b.chunk.js"
+  },
+  {
+    "revision": "9dd5a849241ada4f6ac8",
+    "url": "/foodiesah/static/js/runtime~main.3c8403d8.js"
+  }
+]);
